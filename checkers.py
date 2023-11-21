9ca6b03e1b46b973bcb18d336edf97816b723a1c
George Torres <george.torres.641@gmail.com>
2023-11-20 16:26:00 -0800

    Merged comments and reset

--- checkers.py
+++ checkers.py
@@ -16,26 +16,30 @@ import os
 dirname = os.path.dirname(__file__)
 filename = os.path.join(dirname, 'relative/path/to/file/you/want')
 
-
+# Set the width and number of rows for the game board
 WIDTH = 800
 ROWS = 8
 
+# Load images for the game pieces and board
 RED= pygame.image.load(os.path.join(dirname, 'images/red.png'))
 GREEN= pygame.image.load(os.path.join(dirname, 'images/green.png'))
 
+# Define color constants
 REDKING = pygame.image.load(os.path.join(dirname, 'images/redking.png'))
 GREENKING = pygame.image.load(os.path.join(dirname, 'images/greenking.png'))
 
+# Define color constants
 WHITE = (255,255,255)
 BLACK = (0,0,0)
 ORANGE = (235, 168, 52)
 BLUE = (76, 252, 241)
 
-
+# Initialize Pygame
 pygame.init()
 WIN = pygame.display.set_mode((WIDTH,WIDTH))
 pygame.display.set_caption('Checkers')
 
+# Define a class for each node on the game board
 priorMoves=[]
 class Node:
     def __init__(self, row, col, width):
@@ -51,7 +55,7 @@ class Node:
         if self.piece:
             WIN.blit(self.piece.image, (self.x, self.y))
 
-
+# Function to update and display the game board
 def update_display(win, grid, rows, width):
     for row in grid:
         for spot in row:
@@ -59,7 +63,7 @@ def update_display(win, grid, rows, width):
     draw_grid(win, rows, width)
     pygame.display.update()
 
-
+# Function to create the initial game board grid
 def make_grid(rows, width):
     grid = []
     gap = width// rows
@@ -78,7 +82,7 @@ def make_grid(rows, width):
             grid[i].append(node)
     return grid
 
-
+# Function to draw the game board grid
 def draw_grid(win, rows, width):
     gap = width // ROWS
     for i in range(rows):
@@ -86,7 +90,7 @@ def draw_grid(win, rows, width):
         for j in range(rows):
             pygame.draw.line(win, BLACK, (j * gap, 0), (j * gap, width))
 
-
+# Class representing a game piece
 class Piece:
     def __init__(self, team):
         self.team=team
@@ -96,7 +100,7 @@ class Piece:
     def draw(self, x, y):
         WIN.blit(self.image, (x,y))
 
-
+# Function to get the node position based on mouse coordinates
 def getNode(grid, rows, width):
     gap = width//rows
     RowX,RowY = pygame.mouse.get_pos()
@@ -104,7 +108,7 @@ def getNode(grid, rows, width):
     Col = RowY//gap
     return (Col,Row)
 
-
+# Function to reset colors on the game board
 def resetColours(grid, node):
     positions = generatePotentialMoves(node, grid)
     positions.append(node)
@@ -113,15 +117,18 @@ def resetColours(grid, node):
         nodeX, nodeY = colouredNodes
         grid[nodeX][nodeY].colour = BLACK if abs(nodeX - nodeY) % 2 == 0 else WHITE
 
+# Function to highlight potential moves on the game board
 def HighlightpotentialMoves(piecePosition, grid):
     positions = generatePotentialMoves(piecePosition, grid)
     for position in positions:
         Column,Row = position
         grid[Column][Row].colour=BLUE
 
+# Function to get the opposite team color
 def opposite(team):
     return "R" if team=="G" else "G"
 
+# Function to generate potential moves for a given piece position
 def generatePotentialMoves(nodePosition, grid):
     checker = lambda x,y: x+y>=0 and x+y<8
     positions= []
@@ -149,6 +156,7 @@ def generatePotentialMoves(nodePosition, grid):
 """
 Error with locating possible moves row col error
 """
+# Function to highlight the selected piece and its potential moves
 def highlight(ClickedNode, Grid, OldHighlight):
     Column,Row = ClickedNode
     Grid[Column][Row].colour=ORANGE
@@ -157,6 +165,7 @@ def highlight(ClickedNode, Grid, OldHighlight):
     HighlightpotentialMoves(ClickedNode, Grid)
     return (Column,Row)
 
+# Function to move a game piece on the board
 def move(grid, piecePosition, newPosition):
     resetColours(grid, piecePosition)
     newColumn, newRow = newPosition
@@ -166,12 +175,14 @@ def move(grid, piecePosition, newPosition):
     grid[newColumn][newRow].piece=piece
     grid[oldColumn][oldRow].piece = None
 
+    # Check for king status and update piece type and image
     if newColumn==7 and grid[newColumn][newRow].piece.team=='R':
         grid[newColumn][newRow].piece.type='KING'
         grid[newColumn][newRow].piece.image=REDKING
     if newColumn==0 and grid[newColumn][newRow].piece.team=='G':
         grid[newColumn][newRow].piece.type='KING'
         grid[newColumn][newRow].piece.image=GREENKING
+    # Check for capturing move and remove captured piece
     if abs(newColumn-oldColumn)==2 or abs(newRow-oldRow)==2:
         grid[int((newColumn+oldColumn)/2)][int((newRow+oldRow)/2)].piece = None
         return grid[newColumn][newRow].piece.team
@@ -204,7 +215,7 @@ def reset_game(grid):
             elif abs(i + j) % 2 == 0 and i > 4:
                 grid[i][j].piece = Piece('G')
 
-
+# Main function to run the game loop
 def main(WIDTH, ROWS):
     grid = make_grid(ROWS, WIDTH)
     highlightedPiece = None
